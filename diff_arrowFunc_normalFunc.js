420b2bdebc80ec152f8eb251c5e5c05927289e7d
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2026-06-14 09:58:09 +0530

    Day 3

---
+++ diff_arrowFunc_normalFunc.js
@@ -0,0 +1,23 @@
+function f1(){
+    let h1=12
+    console.log(this.h1)
+}
+f1()
+
+const obj={
+    h1:12,
+     fu (){
+        console.log(this.h1)
+    }
+}
+obj.fu()
+
+const obj2={
+    h1:13,
+    f:()=>{
+        console.log(this.h1)
+    }
+}
+obj2.f()
+
+//"this" refers to object property not to local variable or the object itself 
