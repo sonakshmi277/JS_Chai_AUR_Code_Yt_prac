155f1cd176ea546d68e0468bf673158957873299
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2026-06-17 15:43:25 +0530

    Day 5

--- promise.js
+++ promise.js
@@ -6,3 +6,21 @@ const p=new Promise(function(resolve,reject){
 }).then(function(){
     console.log("Async 2 solved")
 })
+
+const p2=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task 1")
+        resolve("hey")
+    },1000)
+
+})
+async function consumePromise(ans){
+    try{
+        const res=await p2
+        console.log(res)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+consumePromise()
