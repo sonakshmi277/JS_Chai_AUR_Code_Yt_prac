abd972b6adafa51f1eb823d94486091016bdaa83
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2026-06-17 20:24:05 +0530

    Day 5

--- To-do/ch.js
+++ To-do/ch.js
@@ -1,5 +1,6 @@
 const ini = document.querySelector('#buttons')
-
+const tog = document.querySelector("#toggle")
+const parent = document.getElementById("body")
 ini.addEventListener('click', function (e) {
     console.log("button pressed")
     const d = document.createElement('div')
@@ -8,11 +9,11 @@ ini.addEventListener('click', function (e) {
     d.style.color = "white"
     d.style.backgroundColor = "orange"
     d.style.margin = "12px"
-
-    parent = document.getElementById("body")
+    d.style.display = "flex"
+    d.style.justifyContent = "space-between"
+    d.style.alignItems = "center"
     parent.appendChild(d)
-    
-    
+
     const delBtn = document.createElement("button")
     delBtn.textContent = "-"
     delBtn.addEventListener("click", function () {
@@ -22,4 +23,16 @@ ini.addEventListener('click', function (e) {
     d.appendChild(delBtn);
 
 });
+tog.addEventListener('click', function (e) {
+    console.log("done")
+    if (parent.style.backgroundColor === "white") {
+        parent.style.backgroundColor = "black"
+    }
+    else {
+        parent.style.backgroundColor = "white"
+    }
+
+})
+
+
 
